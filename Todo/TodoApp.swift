723e55d09c7151690f86b4f6c3097872861cb993
Mike Goodspeed <mikegoodspeed@users.noreply.github.com>
2023-01-01 13:42:19 -0600

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Mike Goodspeed on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
